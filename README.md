fbe5af0b66b292895f05117e0032f854db9ad233
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2022-06-16 13:11:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# mkdocs-fix-github-wiki-url-plugin
+
+# Install
+
+```
+pip install mkdocs-fix-github-wiki-url-plugin
+```
+
+Edit your mkdocs configuration file to enable the plugin:
+
+```
+plugins:
+  - search
+  - ezlinks
+  - mkdocs-fix-github-wiki-url-plugin
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name="mkdocs-fix-github-wiki-url-plugin",
+    version="0.1.20",
+    author="Quentin Escarbajal",
+    author_email="xiaoxiao921@hotmail.fr",
+    url="https://github.com/xiaoxiao921/mkdocs-fix-github-wiki-url-plugin",
+    description="",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        "mkdocs>=1.1.0,<2"
+    ],
+    include_package_data=True,
+    python_requires='>=3.6',
+    entry_points={
+        'mkdocs.plugins': [
+            'mkdocs-fix-github-wiki-url-plugin = mkdocs_fix_github_wiki_url_plugin.plugin:Plugin',
+        ]
+    }
+
+)
